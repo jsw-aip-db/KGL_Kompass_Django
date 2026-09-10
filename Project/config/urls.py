692b9489bca2bbea6733da506413ea90096d33df
ypbautista <191103002+ypbautista@users.noreply.github.com>
2026-09-10 12:51:46 +0200

    Integrate email functionality and storage configuration; add PDF generation and notification services

--- Project/config/urls.py
+++ Project/config/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 import django
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     path("fragebogen/", include("fragebogen.urls")),
     path("portal/", include("betreuer_portal.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
